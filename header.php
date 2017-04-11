f661b02c14655537048256b6bb7ccf12cafbe260
Florian NICOLAS <florian.nicolas62@free.fr>
2017-04-11 20:32:09 +0200

    Remove zero.css

--- header.php
+++ header.php
@@ -11,7 +11,6 @@
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 
     <link rel="stylesheet" type="text/css" href="/<?= PROJECT_NAME ?>/css/bootstrap.css" />
-		<link rel="stylesheet" type="text/css" href="/<?= PROJECT_NAME ?>/css/zero.css" />
 		<link rel="stylesheet" type="text/css" href="/<?= PROJECT_NAME ?>/css/index.css" />
 
 		<title>Gestion Lan</title>
