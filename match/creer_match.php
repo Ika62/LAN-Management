288c68a653c98f9da9220123fb03856e6ff593cf
Florian NICOLAS <florian.nicolas62@free.fr>
2017-04-13 22:04:45 +0200

    match creation page layout

--- match/creer_match.php
+++ match/creer_match.php
@@ -6,9 +6,39 @@
 
 		<form action="veto.php" method="POST">
 
+
+
 			<div align="center">
-				<div class="teamcontainer">
-					<div id="choix1" class="choix" >Choix de la première équipe</br> </div>
+
+				<!-- Choix du format de match -->
+
+				<div class="choicecontainer">
+					<div class="choix">Choix du format du match</div>
+					<label>BO1</label><input type="radio" name="format" value="bo1" checked />
+					<label>BO3</label><input type="radio" name="format" value="bo3" />
+					<label>BO5</label><input type="radio" name="format" value="bo5" />
+				</div>
+
+				<!-- Choix du serveur -->
+
+				<div class="choicecontainer">
+					<div class="choix">Choisir le serveur</div>
+					<select name="serveur" size="8">
+
+						<option value="1">Serveur 1</option>
+						<option value="2">Serveur 2</option>
+						<option value="3">Serveur 3</option>
+						<option value="4">Serveur 4</option>
+						<option value="5">Serveur 5</option>
+						<option value="6">Serveur 6</option>
+						<option value="7">Serveur 7</option>
+						<option value="8">Serveur 8</option>
+
+					</select>
+				</div>
+
+				<div class="choicecontainer">
+					<div class="choix" >Choix de la première équipe</br> </div>
 
 					<div class="teamlist">
 						<input type="hidden" name="equipe1" id="equipe1" />
@@ -31,8 +61,8 @@
 				</div>
 				<!-- Choix de la deuxième équipe -->
 
-				<div class="teamcontainer">
-					<div id="choix2" class="choix" >Choix de la deuxième équipe</br> </div>
+				<div class="choicecontainer">
+					<div class="choix" >Choix de la deuxième équipe</br> </div>
 
 					<div class="teamlist">
 					    <!-- L'input c'est pour pouvoir l'envoyer a l'autre page -->
@@ -54,35 +84,15 @@
 					</div>
 					</div>
 				</div>
-			</div>
-			<!-- Choix du serveur -->
 
-			<div id="choixServeur">Choisir le serveur</br>
-				<select name="serveur" id="serveur" size="8">
+				<div class="choicecontainer">
+					<!-- Bouton pour creer le match après toutes les informations -->
 
-					<option value="1">Serveur 1</option>
-					<option value="2">Serveur 2</option>
-					<option value="3">Serveur 3</option>
-					<option value="4">Serveur 4</option>
-					<option value="5">Serveur 5</option>
-					<option value="6">Serveur 6</option>
-					<option value="7">Serveur 7</option>
-					<option value="8">Serveur 8</option>
+					<input name="valider" type = "submit" value = "Créer le match" />
+				</div>
 
-				</select>
 			</div>
 
-			<!-- Choix du format de match -->
-
-			<div id="choixFormat">Choix du format du match</br>
-				<label>BO1<input type="radio" name="format" value="bo1" selected></label>
-				<label>BO3<input type="radio" name="format" value="bo3"></label>
-				<label>BO5<input type="radio" name="format" value="bo5"></label>
-			</div>
-
-			<!-- Bouton pour creer le match après toutes les informations -->
-
-			<input name="valider" type = "submit" value = "Créer le match" />
 
 		</form>
 
